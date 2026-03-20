7b7804bb0437a649829df5f8f343d178312a932b
sreelekha14-tech <anasuyasreelekha84@gmail.com>
2026-03-20 12:52:38 +0530

    Add files via upload
    Added Even or Odd Java Program

---
+++ EvenOrOdd.java
@@ -0,0 +1,17 @@
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=72;
+		if(n%2==0) {
+			System.out.println("n is an Even");
+		}
+		else {
+			System.out.println("n is an Odd");
+		}
+
+	}
+
+}
+Output:
+n is an Even
