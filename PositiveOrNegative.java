8150e216b8ed7c1ba23f12adf9a2b8c0063e9b6a
sreelekha14-tech <anasuyasreelekha84@gmail.com>
2026-03-20 12:54:03 +0530

    Add files via upload
    Added Positive or Negative Java program

---
+++ PositiveOrNegative.java
@@ -0,0 +1,21 @@
+public class PositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=-40;
+		if(n>0) {
+			System.out.println("n is a Positive");
+		}
+		else if(n<0) {
+			System.out.println("n is a Negative");
+			
+		}
+		else {
+			System.out.println("n is a Zero");
+		}
+
+	}
+
+}
+Output:
+n is a Negative
