c18d290ac6b5ada6740a3c12dcd50bcbd9d1eda5
sreelekha14-tech <anasuyasreelekha84@gmail.com>
2026-03-20 12:56:13 +0530

    Add files via upload
    Added Divisible by 5 or 11 Java Program

---
+++ Divisibleby.java
@@ -0,0 +1,19 @@
+public class Divisibleby {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Divisible by 5 or 11;
+		int n=55;
+		if(n%5==0||n%11==0) {
+			System.out.println("n is divisible");
+		}
+		else {
+			System.out.println("n is  Non-divisible");
+			
+		}
+
+	}
+
+}
+Output:
+n is divisible
