ab9eb2413d9ad69206ab762ff53a2e4466d354f6
sreelekha14-tech <anasuyasreelekha84@gmail.com>
2026-03-20 12:55:20 +0530

    Add files via upload
    Added MaxNum Java Program

---
+++ MaxNum.java
@@ -0,0 +1,18 @@
+public class MaxNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=48;
+		int b=27;
+		if(a>b) {
+			System.out.println("The Maximum Number is a:"+a);
+		}
+		else {
+			System.out.println("The Maximum Number is b:"+b);
+		}
+	}
+
+}
+
+Output:
+The Maximum Number is a:48
